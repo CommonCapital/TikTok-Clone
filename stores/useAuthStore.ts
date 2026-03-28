69aa0b9eb012c3e435d7818436afa7c3d8a1f3ca
Nursan Omarov <Nursan.Omarov@audiit.com>
2026-03-28 21:44:02 +0500

    add

--- stores/useAuthStore.ts
+++ stores/useAuthStore.ts
@@ -2,7 +2,7 @@ import { User } from '@/src/types/types';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
-
+import { supabase } from '@/src/lib/supabase';
 
 type AuthStore = {
   user: User | null;
@@ -19,24 +19,68 @@ export const useAuthStore = create<AuthStore>()(
       isAuthenticated: false,
       login: async (email: string, password: string) => {
         try {
-          
+          const { data, error } = await supabase.auth.signInWithPassword({
+            email,
+            password
+          })
 
-        
+          if (data && data.user && !error) {
+            const { user } = data;
+
+            const newUser: User = {
+              id: user.id,
+              email: user.email!,
+              username: user.user_metadata.username
+            }
+
+            set({
+              user: newUser,
+              isAuthenticated: true,
+            })
+          }
         } catch (error) {
           throw error;
         }
       },
       register: async (email: string, password: string, username: string) => {
         try {
-          
+          const { data, error } = await supabase.auth.signUp({
+            email,
+            password,
+            options: {
+              data: {
+                username
+              }
+            }
+          })
 
-       
+          if (data && data.user && !error) {
+            const { user } = data;
+
+            const newUser: User = {
+              id: user.id,
+              email: user.email!,
+              username: user.user_metadata.username
+            }
+
+            set({
+              user: newUser,
+              isAuthenticated: true,
+            })
+          }
         } catch (error) {
           throw error;
         }
       },
       logout: async () => {
-        
+        const { error } = await supabase.auth.signOut();
+
+        if (!error) {
+          set({
+            user: null,
+            isAuthenticated: false,
+          })
+        }
       },
     }),
     {
